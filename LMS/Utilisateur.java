491a1af8ab4a0182db03844623a4e45e15791eae
pepenut <benada07@ecolecatholique.ca>
2024-12-05 15:30:26 -0500

    Added Utilisateur.class

--- LMS/Utilisateur.java
+++ LMS/Utilisateur.java
@@ -1,4 +1,48 @@
 package LMS;
+import java.util.*;
 
 public class Utilisateur {
+    private int id;
+    private String nom;
+    private String email;
+    private List<Livre> livresEmpruntes;
+
+        public Livre(int id, String titre, String auteur, String genre, boolean estEmprunter){
+            this.id = id;
+            this.titre = titre;
+            this.auteur = auteur;
+            this.genre = genre;
+            this.estEmprunter = estEmprunter;
+        }
+        public int getId(){
+            return id;
+        }
+        public String getTitre(){
+            return titre;
+        }
+        public String getAuteur(){
+            return auteur;
+        }
+        public String getGenre(){
+            return genre;
+        }
+        public boolean getEstEmprunter(){
+            return estEmprunter;
+        }
+        public void setId(int newId){
+            this.id = newId;
+        }
+        public void setTitre(String newTitre){
+            this.titre = newTitre;
+        }
+        public void setAuteur(String newAuteur){
+            this.auteur = newAuteur;
+        }
+        public void setGenre(String newGenre){
+            this.genre = newGenre;
+        }
+        public void setEstEmprunter(boolean newEstEmprunter){
+            this.estEmprunter = newEstEmprunter;
+        }
+    }
 }
