1527cb315fb5d09101e6c318d3caa39b65dfa5a4
pepenut <benada07@ecolecatholique.ca>
2024-12-05 15:14:44 -0500

    Added Livre.class

--- Livre.java
+++ Livre.java
@@ -4,5 +4,42 @@ public class Livre {
     private String auteur;
     private String genre;
     private boolean estEmprunter;
-    private
+
+    public Livre(int id, String titre, String auteur, String genre, boolean estEmprunter){
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.genre = genre;
+        this.estEmprunter = estEmprunter;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getTitre(){
+        return titre;
+    }
+    public String getAuteur(){
+        return auteur;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public boolean getEstEmprunter(){
+        return estEmprunter;
+    }
+    public void setId(int newId){
+        this.id = newId;
+    }
+    public void setTitre(String newTitre){
+        this.titre = newTitre;
+    }
+    public void setAuteur(String newAuteur){
+        this.auteur = newAuteur;
+    }
+    public void setGenre(String newGenre){
+        this.genre = newGenre;
+    }
+    public void setEstEmprunter(boolean newEstEmprunter){
+        this.estEmprunter = newEstEmprunter;
+    }
 }
